2baff1065f941848f95f7c8d22c15d3a4788ea70
martinmalinda <malindacz@gmail.com>
2017-05-22 16:37:16 +0200

    add ember-devtools

--- config/environment.js
+++ config/environment.js
@@ -30,6 +30,10 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV['ember-devtools'] = {
+      global: true,
+    };
   }
 
   if (environment === 'test') {
