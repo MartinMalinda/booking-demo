eece4cc0325ddc8d54177434e59cd32973ad7193
martinmalinda <malindacz@gmail.com>
2017-06-01 11:53:20 +0200

    calculate dynamically calendar area size

--- app/components/calendar-area.ts
+++ app/components/calendar-area.ts
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 const {computed, $} = Ember;
 
+//TODO: import this
+const COL_WIDTH = 100;
+const ROW_HEIGHT = 70;
+
 export default Ember.Component.extend({
 
   classNames: ['calendar-area'],
@@ -10,4 +14,16 @@ export default Ember.Component.extend({
     let scrollLeft = this.get('scrollLeft');
     return Ember.String.htmlSafe(`transform: translateX(${scrollLeft}px)`);
   }),
+
+  areaWidth: computed('columnDates.length', function(){
+    return (this.get('columnDates.length') + 2) * COL_WIDTH;
+  }),
+
+  areaHeight: computed('rentals.length', function(){
+    return (this.get('rentals.length') + 3) * ROW_HEIGHT;
+  }),
+
+  calendarInnerStyle: computed('areaHeight', 'areaWidth', function(){
+    return Ember.String.htmlSafe(`width:${this.get('areaWidth')}px;height:${this.get('areaHeight')}px;`);
+  })
 });
